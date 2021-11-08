9662487c128e26b144b14eb4174d4ac95ab661bb
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-08 08:56:24 +0530

    Update converter.py

--- abhishek/converter/converter.py
+++ abhishek/converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from abhishek.helpers.errors import FFmpegReturnCodeError
+from abhishek.snehabhi.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
