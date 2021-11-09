27878706b2afd4ccce6faca4e41459c1ef01a644
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-09 11:23:52 +0530

    Update abhi.py

--- abhishek/abhi/abhi.py
+++ abhishek/abhi/abhi.py
@@ -1,9 +1,9 @@
 from abhishek import rohit
-from abhishek import config
+from abhishek.config import API_HASH, API_ID, SESSION_SESSION
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from pytgcalls.types import Update
-from pytgcalls.types.input_stream import InputAudioStream
+
+
 
 
 
@@ -12,19 +12,19 @@ pytgcalls = PyTgCalls(client)
 
 
 @pytgcalls.on_stream_end()
-async def on_stream_end(client: PyTgCalls, update: Update) -> None:
-    chat_id = update.chat_id
+def on_stream_end(chat_id: int) -> None:
+    
     queues.task_done(chat_id)
 
     if queues.is_empty(chat_id):
-        await pytgcalls.leave_group_call(chat_id)
+        pytgcalls.leave_group_call(chat_id)
     else:
-        await pytgcalls.change_stream(
+        pytgcalls.change_stream(
             chat_id, 
-            InputAudioStream(
-                queues.get(chat_id)["file"],
-            ),
-        )
+           
+                queues.get(chat_id)["file"])
+            
+        
 
 
-run = pytgcalls.start
+run = pytgcalls.run
